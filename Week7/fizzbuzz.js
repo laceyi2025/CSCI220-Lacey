afe97127bf7bd832d2b818fba34091806119e8a1
Isaiah Lacey <laceyi2025@gmail.com>
2021-02-22 18:12:17 -0500

    FizzBuzz

--- Week7/fizzbuzz.js
+++ Week7/fizzbuzz.js
@@ -1,2 +1,6 @@
-for(let i=0;i<100;)console.log((++i%3?'':'fizz')+(i%5?'':'buzz')||i)
-
+for (var i=1; i < 101; i++){
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+}
