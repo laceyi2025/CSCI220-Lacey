83eeb1e457095041bd6434b465ca212d51336999
Isaiah Lacey <laceyi2025@gmail.com>
2021-02-24 18:04:12 -0500

    commits

--- Week7/2-24-21Examples.js
+++ Week7/2-24-21Examples.js
@@ -2,7 +2,8 @@
 output = min(1,2);
 console.log(output);
 
-function min (x,y){
+function min (x,y)
+{
     if (x===y)
     {
         return "The Numbers are Equal";
@@ -14,5 +15,16 @@ function min (x,y){
     else
     {
         return y;
+    }
+}
+
+console.log(range(1,10));
+
+funtionrange(start,end)
+{
+    myArray= new Array();
+    for (num=start; num <=end; num++)
+    {
+        myArray[num]
     }
 }
