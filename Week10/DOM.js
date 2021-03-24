3540e670f08aeed177e93e797a1e5cbc40e93e56
Isaiah Lacey <laceyi2025@gmail.com>
2021-03-24 16:30:01 -0400

    examples

--- Week10/DOM.js
+++ Week10/DOM.js
@@ -1,5 +1,33 @@
-// Select the element and store it in a variable.
-var el = document.getElementById('Paragraph');
 
-// Change the value of the name attribute.
-el.Name = 'cool';
+
+var paragraph = document.getElementById("1");
+
+console.log(paragraph.textContent);
+
+//changing data
+
+paragraph.textContent = "New Data";
+
+document.getElementById("1").textContent = paragraph.textContent;
+
+//create element
+
+
+// Create a new element and store it in a variable Ordered List.
+var newOL = document.createElement('ol');
+var positionOL = document.getElementsByTagName('body')[0];
+positionOL.appendChild(newOL);
+
+
+
+function AddListItem()
+{
+// Create a new element and store it in a variable List. Item
+var newLI = document.createElement('li');
+var input = document.getElementById("input").textContent;
+var newObject = document.createTextNode(input);
+var positionLI = document.getElementsByTagName('ol')[0];
+// Insert the new element into its position.
+newLI.appendChild(newObject);
+positionLI.appendChild(newLI);
+}
