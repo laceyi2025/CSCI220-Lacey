a4142ea0e92798b554b0d9eff50963585e2a56f3
Isaiah Lacey <laceyi2025@gmail.com>
2021-04-09 10:07:01 -0400

    First edits of week 12 Assignment

---
+++ Week12/Week12Assignment.js
@@ -0,0 +1,20 @@
+$('form').css({
+    'background-color': '#0277bd',
+    'border': '2px solid black',
+    'color': 'black'
+})
+
+$('input').css({
+    'background-color': 'chocolate',
+    'color': 'white'
+})
+
+$('label').css({
+    'color': 'white'
+})
+
+$('table').css({
+    'background-color': 'chocolate',
+    'border-color': '#0277bd',
+    'color': 'white'
+})
