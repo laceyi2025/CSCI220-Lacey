9e75336874557604066e29576e360420894aba19
Isaiah Lacey <laceyi2025@gmail.com>
2021-04-27 16:26:17 -0400

    Loading data through javascript and design is done

--- Final/ShoppingCartScript.js
+++ Final/ShoppingCartScript.js
@@ -1,1 +1,26 @@
-document.getElementById("")
+//set tax rates
+var taxRate = .07
+
+    //item 1 information
+    document.getElementById("product1").innerHTML = item1.productName
+    document.getElementById("description1").innerHTML = item1.description
+    document.getElementById("price1").innerHTML = item1.price
+    document.getElementById("quantity1").innerHTML = item1.available
+
+    //item 2 information
+    document.getElementById("product2").innerHTML = item2.productName
+    document.getElementById("description2").innerHTML = item2.description
+    document.getElementById("price2").innerHTML = item2.price
+    document.getElementById("quantity2").innerHTML = item2.available
+
+    //item 3 information
+    document.getElementById("product3").innerHTML = item3.productName
+    document.getElementById("description3").innerHTML = item3.description
+    document.getElementById("price3").innerHTML = item3.price
+    document.getElementById("quantity3").innerHTML = item3.available
+
+    //item 4 information
+    document.getElementById("product4").innerHTML = item4.productName
+    document.getElementById("description4").innerHTML = item4.description
+    document.getElementById("price4").innerHTML = item4.price
+    document.getElementById("quantity4").innerHTML = item4.available
