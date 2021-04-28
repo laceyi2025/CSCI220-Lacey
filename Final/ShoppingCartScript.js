05f24c929d67ec951d67e00184fb6d3565b770e1
Isaiah Lacey <laceyi2025@gmail.com>
2021-04-27 22:07:08 -0400

    WorkingWellNeed to fix quantity

--- Final/ShoppingCartScript.js
+++ Final/ShoppingCartScript.js
@@ -1,58 +1,60 @@
     //item 1 information
     document.getElementById("product1").innerHTML = item1.productName;
     document.getElementById("description1").innerHTML = item1.description;
-    document.getElementById("price1").innerHTML = item1.price;
+    document.getElementById("price1").innerHTML = "$"+item1.price;
     document.getElementById("quantity1").innerHTML = item1.available;
 
     //item 2 information
     document.getElementById("product2").innerHTML = item2.productName;
     document.getElementById("description2").innerHTML = item2.description;
-    document.getElementById("price2").innerHTML = item2.price;
+    document.getElementById("price2").innerHTML = "$"+item2.price;
     document.getElementById("quantity2").innerHTML = item2.available;
 
     //item 3 information
     document.getElementById("product3").innerHTML = item3.productName;
     document.getElementById("description3").innerHTML = item3.description;
-    document.getElementById("price3").innerHTML = item3.price;
+    document.getElementById("price3").innerHTML = "$"+item3.price;
     document.getElementById("quantity3").innerHTML = item3.available;
 
     //item 4 information
     document.getElementById("product4").innerHTML = item4.productName;
     document.getElementById("description4").innerHTML = item4.description;
-    document.getElementById("price4").innerHTML = item4.price;
+    document.getElementById("price4").innerHTML = "$"+item4.price;
     document.getElementById("quantity4").innerHTML = item4.available;
   
-    var item1Price = document.getElementById("price1").value;
-    var item1Quantity = document.getElementById("quantity1").value;
-    var item1Total = (item1Price * item1Quantity).toFixed(2) - 0;
 
-    var item2Price = document.getElementById("price2").value;
-    var item2Quantity = document.getElementById("quantity2").value;
-    var item2Total = (item2Price * item2Quantity).toFixed(2) - 0;
+    var removeCartItemButtons = document.getElementsByClassName('btnClear')
 
-    var item3Price = document.getElementById("price3").value;
-    var item3Quantity = document.getElementById("quantity3").value;
-    var item3Total = (item3Price * item3Quantity).toFixed(2) - 0;
-
-    var item4Price = document.getElementById("price4").value;
-    var item4Quantity = document.getElementById("quantity4").value;
-    var item4Total = (item4Price * item4Quantity).toFixed(2) - 0;
-    
-    var taxRate = .07
-    var subtotal = item1Total+item2Total+item3Total+item4Price
-    var tax = subtotal * taxRate
-    var total = subtotal + tax
-
-    window.onload=function updateTotals(){
-        
-        
-        document.getElementById("subtotal").innerHTML = subtotal;
-        document.getElementById("tax").innerHTML = tax;
-        document.getElementById("total").innerHTML = total;
-
+    for (var i = 0; i < removeCartItemButtons.length; i++){
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', function(event){
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
+        })
     }
 
 
+    function updateCartTotal(){
+        var cartItemContainer = document.getElementsByClassName('cartItem')[0]
+        var cartRows = cartItemContainer.getElementsByClassName('cartRow')
+        var total = 0
+        
+
+        for (var i = 0; i < cartRows.length; i++){
+            var cartRow = cartRows[i]
+            var priceElement = cartRow.getElementsByClassName('price')[0]
+            var quantityElement = cartRow.getElementsByClassName('quantityUpdate')[0]
+            var price = parseFloat(priceElement.innerText.replace('$',''))
+            var quantity = quantityElement.value
+            
+            total = total + (price*quantity)
+           
+
+        }
+
+        document.getElementsByClassName('subtotal')[0].innerText = '$'+total
+       }
     
 
     
